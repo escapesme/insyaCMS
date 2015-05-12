aedd54a1ca7ccc9f58befb168a4d291d609f47f0
empcl_000 <empcland@gmail.com>
2015-05-12 14:26:28 +0300

    bcvbcvb

---
+++ admin/components/com_newsletter/index.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * Project :Scape
+ * Version 2.0.1 2012
+ * Copyright E-Scapes IT Solutions
+ * Authors: IMI, AD, AMV
+ * 
+ */
+
+
+
+global $lib;
+
+
+
+
+
+
+echo $lib->adminEng->getComponentMain();
+?>
+
