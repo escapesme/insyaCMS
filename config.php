1bd422843e83d26dc7d0b997c89f8bdc26b64979
empcl_000 <empcland@gmail.com>
2015-05-12 12:48:39 +0300

    bcvbcvb

--- config.php
+++ config.php
@@ -1,11 +1,16 @@
 <?php
-
+/*
 $setting["db_name"] = 'insya';
 $setting["db_user"] = 'root';
 $setting["db_pass"] = '';
 $setting["db_host"] = 'localhost';
 
+*/
 
+$setting["db_name"] = 'escapesm_gitfaiz';
+$setting["db_user"] = 'escapesm_newfaiz';
+$setting["db_pass"] = 'newfaiz5152';
+$setting["db_host"] = 'localhost';
 
 $setting["siteTitle"] = 'E-Scapes Main Repository';
 $setting["admin_mail"] = 'info@e-scapes.me';
